1e3c29b09c7d325ccce242fc660d34e6f4bd6c2b
opwins2 <patersono3812@my.sduhsd.net>
2026-04-07 20:47:06 +0000

    play test

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // add javascript here
+let answer = 0;
+let guessCount = 0;
+let totalWins = 0;
+let totalGuesses = 0;
+let scores = 0;
+// Player name
+let playerName = prompt("Enter your name:");
+
+// Play
+// get level
+document.getElementById("playBtn").addEventListener("click", function() {
+    let radios = document.getElementsByName("level");
+    let range = 3;
+    for (let i=0; i < radios.length; i++){
+        if(radios[i].checked){
+            range = parseint(radios[i].value);
+        }
+    }
+answer = Math.floor(Math.random() * range) + 1;
+
+document.getElementById("msg").textContent = playerName + ", guess a number between 1 and " + range;
+document.getElementById("guess").value="";
+document.getElementById("guessBtn").disabled = false
+document.getElementById("giveUpBtn").disabled = false
+document.getElementById("playBtn").disabled = true
+});
